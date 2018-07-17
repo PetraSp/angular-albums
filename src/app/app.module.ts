3840e81857098fa583773aea77dda6b5e99720f6
Petra Spirkova <petra.spirkova@netcentric.biz>
2018-07-17 18:19:30 +0200

    add albums-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AlbumsPageComponent } from './pages/albums-page/albums-page.component';
+
+const routes: Routes = [ 
+  { path: '', redirectTo: '/albums', pathMatch: 'full' },
+  { path: 'albums', component: AlbumsPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
